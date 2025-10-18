822434402fef8949c08a83cfef48f75a56f84276
Enveloping <2421796684@qq.com>
2025-10-18 15:17:35 +0800

    把 audioDuration 的类型从Date改为String

--- src/main/java/com/voice/tts/pojo/entity/AudioInfoDo.java
+++ src/main/java/com/voice/tts/pojo/entity/AudioInfoDo.java
@@ -3,11 +3,10 @@ package com.voice.tts.pojo.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.context.annotation.Primary;
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -19,7 +18,7 @@ public class AudioInfoDo {
   public int userId;
   public String audioName;
   public Date createTime;
-  public Date audioDuration;
+  public String audioDuration;
   public int speed;
   public int emotion;
 }
